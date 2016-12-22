d22ccecbe5903d23748a7714d3a1c106857a24d3
CarrBrent <958240037@qq.com>
2016-12-22 15:25:53 +0800

    add startingroupevaluate fun add endingroupevaluate fun add startoutgroupevaluate fun add endoutgroupevaluate fun

--- src/app/ui/activity/evaluate/OutGroupEvaluateActivity.java
+++ src/app/ui/activity/evaluate/OutGroupEvaluateActivity.java
@@ -1,5 +1,12 @@
 package app.ui.activity.evaluate;
 
+import com.lidroid.xutils.HttpUtils;
+import com.lidroid.xutils.exception.HttpException;
+import com.lidroid.xutils.http.RequestParams;
+import com.lidroid.xutils.http.ResponseInfo;
+import com.lidroid.xutils.http.callback.RequestCallBack;
+import com.lidroid.xutils.http.client.HttpRequest;
+
 import myclass.manager.teacher.R;
 import android.content.Intent;
 import android.os.Bundle;
@@ -11,6 +18,7 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 import app.ui.TitleActivity;
+import app.util.BaseInfo;
 
 public class OutGroupEvaluateActivity extends TitleActivity implements OnClickListener{
 	/* (non-Javadoc)
@@ -23,6 +31,13 @@ public class OutGroupEvaluateActivity extends TitleActivity implements OnClickLi
 	private TextView tv; 
 	private Button start; 
 	private Button reset; 
+	private String startoutgroupevaluate = "startoutgroupevaluate.do";
+	private String endoutgroupevaluate = "endoutgroupevaluate.do";
+	private String starturl;
+	private String endurl;
+
+	private HttpUtils http = new HttpUtils();
+	private BaseInfo baseInfo;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 
@@ -30,6 +45,13 @@ public class OutGroupEvaluateActivity extends TitleActivity implements OnClickLi
 		setContentView(R.layout.activity_evaluate_outgroup);
 		showBackwardView(R.string.button_backward, true);
 
+		// 设置缓存1秒,1秒内直接返回上次成功请求的结果
+		http.configCurrentHttpCacheExpiry(1000);
+		
+		
+		baseInfo = (BaseInfo)getApplication();
+		starturl = baseInfo.getUrl()+startoutgroupevaluate;
+		endurl = baseInfo.getUrl()+endoutgroupevaluate;
 		et = (EditText) findViewById(R.id.timer);
 		tv = (TextView) findViewById(R.id.lefttime);
 		start = (Button) findViewById(R.id.btnStart);
@@ -73,28 +95,83 @@ public class OutGroupEvaluateActivity extends TitleActivity implements OnClickLi
 	public void start(){
 		String time = et.getText().toString();
 		if (time.length() != 0) {
-			int millisInFuture=Integer.parseInt(time);
-			start.setClickable(false);
-
-			timer = new CountDownTimer(millisInFuture*1000, 1000) {
-
-				@Override
-				public void onTick(long millisUntilFinished) {
-					// TODO Auto-generated method stub
-					tv.setText( millisUntilFinished / 1000 + " S");
-				}
-
-				@Override
-				public void onFinish() {
-					// TODO Auto-generated method stub
-					tv.setText("评价时间到"); 
-				}
-			};
-			timer.start();
+			startoutgroupevaluate(time);
 		}else {
 			Toast.makeText(OutGroupEvaluateActivity.this, "请输入评价时间",1).show();;
 		}
-		
+
+	}
+	//开始评价的时候调用该方法，客户端向服务器发送请求，设置开始评价标签，学生端才能进行评价。
+	public void startoutgroupevaluate(String time) {
+		RequestParams params = new RequestParams();
+		params.addQueryStringParameter("seId",Integer.toString(seId));
+		http.send(HttpRequest.HttpMethod.GET,
+				starturl,
+				params,
+				new RequestCallBack<String>() {
+
+			@Override
+			public void onStart() {
+			}
+
+			@Override
+			public void onLoading(long total, long current, boolean isUploading) {
+			}
+
+			@Override
+			//访问成功之后执行该方法
+			public void onSuccess(ResponseInfo<String> responseInfo) {
+				//访问成功之后设置倒计时
+				final String time = et.getText().toString();
+				int millisInFuture=Integer.parseInt(time);
+				start.setClickable(false);
+				timer = new CountDownTimer(millisInFuture*1000, 1000) {
+
+					@Override
+					public void onTick(long millisUntilFinished) {
+						// TODO Auto-generated method stub
+						tv.setText( millisUntilFinished / 1000 + " S");
+					}
+
+					@Override
+					public void onFinish() {
+						// TODO Auto-generated method stub
+						//倒计时结束之后调用，向服务器发送请求，关闭评价功能
+						endtoutgroupevaluate(time);
+					}
+
+					private void endtoutgroupevaluate(String time) {
+						// TODO Auto-generated method stub
+						RequestParams params = new RequestParams();
+						params.addQueryStringParameter("seId",Integer.toString(seId));
+						http.send(HttpRequest.HttpMethod.GET,
+								endurl,
+								params,
+								new RequestCallBack<String>() {
+
+							@Override
+							public void onStart() {
+							}
+							@Override
+							public void onLoading(long total, long current, boolean isUploading) {
+							}
+							@Override
+							public void onSuccess(ResponseInfo<String> responseInfo) {
+								tv.setText("评价时间到");
+							}
+							@Override
+							public void onFailure(HttpException error, String msg) {
+							}
+						});
+					}
+				};
+				timer.start();
+			}
+			@Override
+			public void onFailure(HttpException error, String msg) {
+			}
+		});
+
 	}
 
 	public void reset() { 
