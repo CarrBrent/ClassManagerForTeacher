62da4e63fde588d25039eb57e005807497cb571f
CarrBrent <958240037@qq.com>
2017-06-03 18:52:28 +0800

    修改所有按钮块的图标和样式

--- src/app/util/BaseInfo.java
+++ src/app/util/BaseInfo.java
@@ -13,7 +13,8 @@ public class BaseInfo extends Application{
     }  
     @Override  
     public void onCreate(){  
-        url = "http://219.216.65.246:8080/spring/";  
+        //url = "http://219.216.65.246:8080/spring/";
+    	url = "http://192.168.56.1:8080/IB/"; 
         super.onCreate();  
     }  
 }  
