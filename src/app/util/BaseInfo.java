14fe6adc455a3c31cccc29ad4b26b4282eceb42c
CarrBrent <958240037@qq.com>
2017-06-03 20:16:33 +0800

    修改按钮块的色调

--- src/app/util/BaseInfo.java
+++ src/app/util/BaseInfo.java
@@ -14,7 +14,7 @@ public class BaseInfo extends Application{
     @Override  
     public void onCreate(){  
         //url = "http://219.216.65.246:8080/spring/";
-    	url = "http://192.168.56.1:8080/IB/"; 
+    	url = "http://192.168.1.13:8080/IB/"; 
         super.onCreate();  
     }  
 }  
